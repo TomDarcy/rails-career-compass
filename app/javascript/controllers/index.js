06be61d49a3bbe6a1b8affd595ef4ad92abc8239
Anna Peysikova <anpey21@gmail.com>
2023-03-15 07:31:16 +0800

    Wed morning

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -12,3 +12,13 @@ application.register("hello", HelloController)
 
 // const application = Application.start()
 // application.register('scroll-to', ScrollTo)
+
+const pages = document.querySelectorAll('.page')
+const translateAmount = 100
+let translate = 0
+
+slide = (direction) => {
+  direction === "next" ? translate -= translateAmount : translate += translateAmount
+    pages.forEach((pages => (pages.style.transform = `translateX(${translate}%)`)
+))
+}
