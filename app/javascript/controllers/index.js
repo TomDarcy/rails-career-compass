7aa380d6910f77d8cd2601bbab55a16c70ce64b1
Anna Peysikova <anpey21@gmail.com>
2023-03-15 07:51:04 +0800

    removed JS

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -12,13 +12,3 @@ application.register("hello", HelloController)
 
 // const application = Application.start()
 // application.register('scroll-to', ScrollTo)
-
-const pages = document.querySelectorAll('.page')
-const translateAmount = 100
-let translate = 0
-
-slide = (direction) => {
-  direction === "next" ? translate -= translateAmount : translate += translateAmount
-    pages.forEach((pages => (pages.style.transform = `translateX(${translate}%)`)
-))
-}
